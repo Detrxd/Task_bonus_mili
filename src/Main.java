b182770928f6a9015b449ac638072a9fb091a896
Ilya Filimonov <ilyafil1996@yandex.ru>
2021-05-18 14:24:48 +0300

    Rework task

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,8 @@
 public class Main {
-        public static void main(String[] args) {
-            // Объявляете переменные для входных данных и
-            // параметров программы: одну для хранения
-            // стоимости билета, другую для хранения количества
-            // рублей для одной бонусной милли
-
-            // Рассчитываете количество бонусных милль, используя
-            // значения заведённых переменных. Ответ сохраняете в
-            // новую переменную и выводите на экран
+    public static void main(String[] args) {
         int price_ticket = 995;
         int bonus = 20;
-        int mili =  price_ticket/bonus;
-                    System.out.println("Количество Ваших бесплатных милей = " + mili);
-        }
+        int mili = price_ticket / bonus;
+        System.out.println("Количество Ваших бесплатных милей = " + mili);
     }
+}

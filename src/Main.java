787a542035944968dfdb413c778148f7cf9b8394
Ilya Filimonov <ilyafil1996@yandex.ru>
2021-05-19 09:00:31 +0300

    Refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price_ticket = 995;
+        int priceTicket = 995;
         int bonus = 20;
-        int mili = price_ticket / bonus;
+        int mili = priceTicket / bonus;
         System.out.println("Количество Ваших бесплатных милей = " + mili);
     }
 }
